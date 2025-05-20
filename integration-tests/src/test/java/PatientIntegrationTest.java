35637be3bc2e316542b40dda47db086655b3f98a
Sanjay Mehta <sanjay.mehta@hidubai.com>
2025-05-20 16:23:16 +0400

    Integration Test added using Rest Assured

---
+++ integration-tests/src/test/java/PatientIntegrationTest.java
@@ -0,0 +1,41 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class PatientIntegrationTest {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnPatientsWithValidToken() {
+        String loginPayload = """
+                {
+                    "email" : "testuser@test.com",
+                    "password" : "password123"
+                }
+                """;
+
+        String token = given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("token");
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/api/patients")
+                .then()
+                .statusCode(200)
+                .body("patients", notNullValue());
+    }
+}
